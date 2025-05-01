544f0693afb44c66a2d21c5d15dd48607026241b
Kaique Borges <rique.kaique@gmail.com>
2025-04-30 21:51:53 -0300

    test: :test_tube: red: should not authenticate user if password is wrong

--- src/use-cases/organization/authenticate.use-case.spec.ts
+++ src/use-cases/organization/authenticate.use-case.spec.ts
@@ -1,15 +1,67 @@
 import { describe, expect, it } from 'vitest';
 import { AuthenticateUseCase } from './authenticate.use-case';
+import { InvalidCredentialsError } from '../errors/invalid-credentials.error';
+import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations.repository';
+
+let organizationsRepository = new InMemoryOrganizationsRepository();
 
 describe('Authenticate Use Case', () => {
-  it('should authenticate a user', () => {
-    const authenticateUseCase = new AuthenticateUseCase();
+  it('should authenticate a user', async () => {
+    await organizationsRepository.create({
+      address: 'sample address',
+      email: 'johndoe@example.com',
+      name: 'JS Organization',
+      owner: 'John Doe',
+      passwordHash: 'hashed-password',
+      phone: '(99) 9 9999-9999',
+      zipCode: '55555-555',
+    });
+
+    const authenticateUseCase = new AuthenticateUseCase(
+      organizationsRepository,
+    );
     const result = authenticateUseCase.execute({
       email: 'johndoe@example.com',
       password: '123456',
     });
-    expect(result).toHaveResolved();
+    await expect(result).resolves.toBe(null);
   });
-  it.todo("should not authenticate a user if email doesn't exist");
-  it.todo('should not authenticate a user if password is wrong');
+
+  it("should not authenticate a user if email doesn't exist", async () => {
+    const organizationsRepository = new InMemoryOrganizationsRepository();
+    const authenticateUseCase = new AuthenticateUseCase(
+      organizationsRepository,
+    );
+
+    await expect(
+      authenticateUseCase.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toThrow(InvalidCredentialsError);
+  });
+
+  it('should not authenticate a user if password is wrong', async () => {
+    organizationsRepository = new InMemoryOrganizationsRepository();
+    await organizationsRepository.create({
+      address: 'sample address',
+      email: 'johndoe@example.com',
+      name: 'JS Organization',
+      owner: 'John Doe',
+      passwordHash: 'hashed-password',
+      phone: '(99) 9 9999-9999',
+      zipCode: '55555-555',
+    });
+
+    const authenticateUseCase = new AuthenticateUseCase(
+      organizationsRepository,
+    );
+
+    await expect(
+      authenticateUseCase.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toThrow(InvalidCredentialsError);
+  });
 });
--- src/use-cases/organization/authenticate.use-case.ts
+++ src/use-cases/organization/authenticate.use-case.ts
@@ -1,8 +1,20 @@
+import { OrganizationsRepository } from '@/repositories/organizations.repository';
+import { InvalidCredentialsError } from '../errors/invalid-credentials.error';
+
 interface AuthenticateUseCaseInput {
   email: string;
   password: string;
 }
 
 export class AuthenticateUseCase {
-  async execute({ email, password }: AuthenticateUseCaseInput) {}
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute({ email, password }: AuthenticateUseCaseInput) {
+    const organizationWithEmail =
+      await this.organizationsRepository.getByEmail(email);
+    if (!organizationWithEmail) {
+      throw new InvalidCredentialsError();
+    }
+    return Promise.resolve(null);
+  }
 }
